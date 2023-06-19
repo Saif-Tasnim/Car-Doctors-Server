925806de1175b104d053838d1ae7434218d62454
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-06-19 23:33:35 +0600

    module-70 crud ot done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -32,9 +32,76 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
+
+        const serviceCollection = client.db("carDoctors").collection('services');
+
+        const bookingCollections = client.db("carDoctors").collection('bookingData');
+
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const options = {
+                projection: { title: 1, price: 1, service_id: 1, img: 1 }
+            }
+
+            const result = await serviceCollection.findOne(query, options);
+
+            res.send(result);
+        })
+
+        app.post('/bookingData', async (req, res) => {
+            const data = req.body;
+            const result = await bookingCollections.insertOne(data);
+            res.send(result);
+        })
+
+        app.get('/bookingData' , async (req,res) => {
+            const req_email = req.query.email;
+            let query = {};
+            
+            if(req_email){
+              query={email:req_email}
+            }
+            const result = await bookingCollections.find(query).toArray();
+            res.send(result);
+        })
+
+        app.delete('/bookingData/:id' , async (req,res) => {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await bookingCollections.deleteOne(query);
+            res.send(result);
+        })
+
+        app.patch('/bookingData/:id', async(req,res) => {
+            const id = req.params.id;
+
+            const filter = {_id: new ObjectId(id)}
+
+            const updatedBooking = req.body;
+
+            const updateDoc = {
+                $set: {
+                   status : updatedBooking.status
+                },
+              };
+            
+            const result = await bookingCollections.updateOne(filter , updateDoc);
+
+            res.send(result);
+        })
+
+
+ } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
