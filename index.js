1943fc2d1aa855b86a6b570847bc8a8b92f6b009
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-06-20 22:06:05 +0600

    live site link upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// jwt access token
+
+
 
 
 // mongo db code start
